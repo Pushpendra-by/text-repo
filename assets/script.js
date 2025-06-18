c8f1d73c90b1335de891ba85791688fbd7dbd508
Pushpendra <chourasiyapushpendra38@gmail.com>
2025-06-18 23:30:33 +0530

    my fronted projects

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let  firstName = 'Ram',
+  lastName = 'kumar',
+age = 33;
+console.log(age);
+console.log(firstName);
+
+// var otherName = 'Pushpendra';
+// console.log(otherName);
+var x = 5,
+y = 7,
+z = x + y;
+confirm.log(z);
